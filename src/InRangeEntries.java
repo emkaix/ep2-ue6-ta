efec9d312265835b987b6d3c60ce6ea2a801ef7c
robaerd <robert.sowula9879@gmail.com>
2018-05-16 20:23:22 +0200

    add 'DataEntry', add 'toSting', changed variablenames and following:
    added class 'DataEntry' which returns an ArrayList containing all DataEntrys in range of an specifc radius
    added toString method in 'DataEntry
    
    changed variable names form 'X' and 'Y' to 'lat' and 'lon'
    
    'main' prints all Entries in Range

---
+++ src/InRangeEntries.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class InRangeEntries {
+
+    public static ArrayList<DataEntry> addDataEntryIfInRange(ArrayList<DataEntry> entries, double pointLat, double pointLon, double radius){
+        ArrayList<DataEntry> inRangeEntries = new ArrayList<DataEntry>();
+        for(int i = 0; i < entries.size(); i++){
+            if(inRadiusRange(pointLat,pointLon,entries.get(i).getLat(),entries.get(i).getLon(),radius)){
+                inRangeEntries.add(entries.get(i));
+            }
+        }
+        return inRangeEntries;
+
+    }
+
+    public static boolean inRadiusRange (double lat1, double lon1, double lat2, double lon2, double radius) {
+        int radiusEarth = 6371;
+        double differenceLat = (degreeToRad(lat2)- degreeToRad(lat1));
+        double differenceLon = (degreeToRad(lon2)- degreeToRad(lon1));
+
+        /*
+        This uses the ‘haversine’ formula to calculate the great-circle distance between two points
+        a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+        c = 2 ⋅ atan2( √a, √(1−a) )
+        d = R ⋅ c
+
+        φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+        note that angles need to be in radians to pass to trig functions!
+         */
+
+        double a = Math.pow(Math.sin(differenceLat/2),2) + Math.cos(degreeToRad(lat1)) * Math.cos(degreeToRad(lat2)) *
+                Math.sin(differenceLon/2) * Math.sin(differenceLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = radiusEarth * c; //distance in km
+
+        return d <= radius ? true : false;
+
+    }
+
+    private static double degreeToRad (double deg){
+        return deg*Math.PI/180;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -6,5 +6,10 @@ public class main {
     public static void main(String[] args) {
         DataReader dr = new DataReader();
         ArrayList<DataEntry> entries = dr.ReadEntriesFromFile(System.getProperty("user.dir") + "/data/junctions.csv");
+        ArrayList<DataEntry> result = InRangeEntries.addDataEntryIfInRange(entries, entries.get(0).getLat(), entries.get(0).getLon(),100);
+        System.out.println(result.toString());
+
     }
+
+
 }
