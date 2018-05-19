6b65ed9e2628b659c4c3c5609ec430ffa5e83056
mkx <k.hahnenkamp@gmail.com>
2018-05-19 12:57:50 +0200

    test QuadTree insert

--- src/main.java
+++ src/main.java
@@ -5,17 +5,20 @@ public class main {
 
     public static void main(String[] args) {
         //das wird mit jeder anderen datenstruktur als parameter auch funktionieren
-        DataReader dr = new DataReader(new SimpleDataCollection());
-        IDataCollection dc = dr.ReadEntriesFromFile();
+        //DataReader dr = new DataReader(new SimpleDataCollection());
+        //IDataCollection dc = dr.ReadEntriesFromFile();
 
         //zweite beispielanfrage laut angabezettel
-        EntryCount counter = dc.inRange(new Vector2D(1818.54657, 5813.29982), 100);
-        System.out.println(counter);
+        //EntryCount counter = dc.inRange(new Vector2D(1818.54657, 5813.29982), 100);
+        //System.out.println(counter);
 
         //vierte abfrage leuat angabezettel
-        EntryCount counter2 = dc.TrainstationsNearAirports(15, 20);
-        System.out.println(counter2.printAirports());
+        //EntryCount counter2 = dc.TrainstationsNearAirports(15, 20);
+        //System.out.println(counter2.printAirports());
 
+        /////////////////////////////////////////// quadtree
+        DataReader dr2 = new DataReader(new QuadTree(new AABB(new Vector2D(0, 0), 100000), 1));
+        IDataCollection dc2 = dr2.ReadEntriesFromFile();
 
 
     }
